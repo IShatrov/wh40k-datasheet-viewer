a82ee002f39d92009a6a661007d176ddb1332c3f
IShatrov <shatrov2004@mail.ru>
2024-10-14 19:53:06 +0300

    Initial commit

---
+++ src/main/java/com/wh/wh40k_datasheet_viewer/Wh40kDatasheetViewerApplication.java
@@ -0,0 +1,13 @@
+package com.wh.wh40k_datasheet_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wh40kDatasheetViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wh40kDatasheetViewerApplication.class, args);
+	}
+
+}
